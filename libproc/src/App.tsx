b36aed7bb24f4703418b73c0b027beed3ab85941
Lucas Fengz <lucasfeng83@gmail.com>
2023-06-04 13:58:36 -0300

    Hello Word, estrutura padrao react

---
+++ libproc/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Page from './pages/home/index'
+
+function App() {
+  return (
+    <>
+      <Page />
+    </>
+  );
+}
+
+export default App;
---
+++ libproc/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Page(){
+    return(
+        <>
+        <h1>LIBPROC #3172</h1>
+        </>
+    )
+}
+
+export default Page
