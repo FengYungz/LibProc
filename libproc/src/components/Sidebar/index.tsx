cbbad0332e973865e02e97d2c3d4ab42be3d5b27
Lucas Fengz <lucasfeng83@gmail.com>
2023-07-25 23:22:12 -0300

    Arrumando paginas e rotas

--- libproc/src/components/Sidebar/index.tsx
+++ libproc/src/components/Sidebar/index.tsx
@@ -24,9 +24,52 @@ const Sidebar = ({active, changeState}: {changeState: (state:boolean) => void, a
     <Container /*sidebar={active}*/>
       <FaTimes onClick={closeSidebar} />  
       <Content>
-        <SidebarItem Icon={FaHome} Text="Home" link="/home" />
-        <SidebarItem Icon={FaEnvelope} Text="Memoria" link="/teste" />
-
+        <SidebarItem Icon={FaHome} Text="Home" link="/" />
+        <SidebarItem Icon={FaEnvelope} Text="AcessoMemoria" link="/topicos/acessoMemoria" />
+        <SidebarItem Icon={FaEnvelope} Text="Arquitetura" link="/topicos/arquitetura" />
+        <SidebarItem Icon={FaEnvelope} Text="ConjuntoInstrucoes" link="/topicos/conjuntoInstrucoes" />
+        <SidebarItem Icon={FaEnvelope} Text="InstrucoesSalto" link="/topicos/salto" />
+        <SidebarItem Icon={FaEnvelope} Text="InstrucoesALU" link="/topicos/alu" />
+        <SidebarItem Icon={FaEnvelope} Text="ReinicioExcecoes" link="/topicos/reinicioExcecoes" />
+        <SidebarItem Icon={FaEnvelope} Text="Coprocessadores" link="/topicos/coprocessadores" />
+        <SidebarItem Icon={FaEnvelope} Text="ConjuntoInstrucoesThumb" link="/topicos/thumb" />
+        <SidebarItem Icon={FaEnvelope} Text="InstrucoesSaltoThumb" link="/topicos/saltoThumb" />
+        <SidebarItem Icon={FaEnvelope} Text="OperacoesAritmeticas" link="/topicos/aritmeticas" />
+        <SidebarItem Icon={FaEnvelope} Text="InstrucoesAcessoMemoria" link="/topicos/loadStore" />
+        <SidebarItem Icon={FaEnvelope} Text="ConjuntoInstrucoesThumb2" link="/topicos/thumb2" />
+        <SidebarItem Icon={FaEnvelope} Text="ExecucaoConjuntoInstrucoesThumb" link="/topicos/execucaoThumb" />
+        <SidebarItem Icon={FaEnvelope} Text="AmbienteGNU" link="/topicos/ambienteGnu" />
+        <SidebarItem Icon={FaEnvelope} Text="AssemblerGNU" link="/topicos/assembler" />
+        <SidebarItem Icon={FaEnvelope} Text="Simbolos" link="/topicos/simbolos" />
+        <SidebarItem Icon={FaEnvelope} Text="Expressoes" link="/topicos/expressoes" />
+        <SidebarItem Icon={FaEnvelope} Text="DefinicaoSecoes" link="/topicos/definicaoSecoes" />
+        <SidebarItem Icon={FaEnvelope} Text="AtribuicaoSimbolos" link="/topicos/atribuicaoSimbolos" />
+        <SidebarItem Icon={FaEnvelope} Text="IntroducaoDados" link="/topicos/introducaoDados" />
+        <SidebarItem Icon={FaEnvelope} Text="CodigoCondicional" link="/topicos/codigoCondicional" />
+        <SidebarItem Icon={FaEnvelope} Text="PosicionarEnderecoAtual" link="/topicos/enderecoAtual" />
+        <SidebarItem Icon={FaEnvelope} Text="MacrosRepeticoes" link="/topicos/macrosRepeticoes" />
+        <SidebarItem Icon={FaEnvelope} Text="GNULD" link="/topicos/gnuld" />
+        <SidebarItem Icon={FaEnvelope} Text="DepuradorGNU" link="/topicos/gdb" />
+        <SidebarItem Icon={FaEnvelope} Text="ExecucaoControleProcesso" link="/topicos/execucaoProcesso" />
+        <SidebarItem Icon={FaEnvelope} Text="Breakpoints" link="/topicos/breakpoints" />
+        <SidebarItem Icon={FaEnvelope} Text="VariaveisConteudoMemoria" link="/topicos/variaveisMemoria" />
+        <SidebarItem Icon={FaEnvelope} Text="ControleInterfaceUsuario" link="/topicos/interfaceUsuario" />
+        <SidebarItem Icon={FaEnvelope} Text="PlacaDesenvolvimento" link="/topicos/placaDesenvolvimento" />
+        <SidebarItem Icon={FaEnvelope} Text="PortasEntradaSaidaGPIO" link="/topicos/gpio" />
+        <SidebarItem Icon={FaEnvelope} Text="GerenciadorInterrupcoes" link="/topicos/gerenciadorInterrupcoes" />
+        <SidebarItem Icon={FaEnvelope} Text="Temporizadores" link="/topicos/temporizadores" />
+        <SidebarItem Icon={FaEnvelope} Text="UARTs" link="/topicos/uarts" />
+        <SidebarItem Icon={FaEnvelope} Text="ImplementacaoLinguagemC" link="/topicos/implementacaoFuncoesC" />
+        <SidebarItem Icon={FaEnvelope} Text="CRuntime" link="/topicos/cRuntime" />
+        <SidebarItem Icon={FaEnvelope} Text="BibliotecaC" link="/topicos/bibliotecaC" />
+        <SidebarItem Icon={FaEnvelope} Text="DadosC" link="/topicos/dadosC" />
+        <SidebarItem Icon={FaEnvelope} Text="ImplementacaoFuncoesC" link="/topicos/implementacaoFuncoesC" />
+        <SidebarItem Icon={FaEnvelope} Text="GNUCompiler" link="/topicos/gcc" />
+        <SidebarItem Icon={FaEnvelope} Text="AmbienteDesenvolvimento" link="/topicos/ambienteDesenvolvimento" />
+        <SidebarItem Icon={FaEnvelope} Text="AtributosFuncaoGCC" link="/topicos/atributosFuncao" />
+        <SidebarItem Icon={FaEnvelope} Text="AssemblerInline" link="/topicos/assemblerInline" />
+        <SidebarItem Icon={FaEnvelope} Text="RuntimeGCC" link="/topicos/runtimeGcc" />
+        <SidebarItem Icon={FaEnvelope} Text="BibliotecaCNewlib" link="/topicos/bibliotecaCNewlib" />
       </Content>
     </Container>
   )
--- libproc/src/routes/index.tsx
+++ libproc/src/routes/index.tsx
@@ -2,14 +2,102 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/home/';
-import Teste from '../pages/teste';
+import AcessoMemoria from '../pages/topicos/acessoMemoria';
+import Arquitetura from '../pages/topicos/arquitetura';
+import ConjuntoInstrucoes from '../pages/topicos/conjuntoInstrucoes';
+import InstrucoesSalto from '../pages/topicos/salto';
+import InstrucoesALU from '../pages/topicos/alu';
+import ReinicioExcecoes from '../pages/topicos/reinicioExcecoes';
+import Coprocessadores from '../pages/topicos/coprocessadores';
+import ConjuntoInstrucoesThumb from '../pages/topicos/thumb';
+import InstrucoesSaltoThumb from '../pages/topicos/saltoThumb';
+import OperacoesAritmeticas from '../pages/topicos/aritmeticas';
+import InstrucoesAcessoMemoria from '../pages/topicos/loadStore';
+import ConjuntoInstrucoesThumb2 from '../pages/topicos/thumb2';
+import ExecucaoConjuntoInstrucoesThumb from '../pages/topicos/execucaoThumb';
+import AmbienteGNU from '../pages/topicos/ambienteGnu';
+import AssemblerGNU from '../pages/topicos/assembler';
+import Simbolos from '../pages/topicos/simbolos';
+import Expressoes from '../pages/topicos/expressoes';
+import DefinicaoSecoes from '../pages/topicos/definicaoSecoes';
+import AtribuicaoSimbolos from '../pages/topicos/atribuicaoSimbolos';
+import IntroducaoDados from '../pages/topicos/introducaoDados';
+import CodigoCondicional from '../pages/topicos/codigoCondicional';
+import PosicionarEnderecoAtual from '../pages/topicos/enderecoAtual';
+import MacrosRepeticoes from '../pages/topicos/macrosRepeticoes';
+import GNULD from '../pages/topicos/gnuld';
+import DepuradorGNU from '../pages/topicos/gdb';
+import ExecucaoControleProcesso from '../pages/topicos/execucaoProcesso';
+import Breakpoints from '../pages/topicos/breakpoints';
+import VariaveisConteudoMemoria from '../pages/topicos/variaveisMemoria';
+import ControleInterfaceUsuario from '../pages/topicos/interfaceUsuario';
+import PlacaDesenvolvimento from '../pages/topicos/placaDesenvolvimento';
+import PortasEntradaSaidaGPIO from '../pages/topicos/gpio';
+import GerenciadorInterrupcoes from '../pages/topicos/gerenciadorInterrupcoes';
+import Temporizadores from '../pages/topicos/temporizadores';
+import UARTs from '../pages/topicos/uarts';
+import ImplementacaoLinguagemC from '../pages/topicos/implementacaoFuncoesC';
+import CRuntime from '../pages/topicos/cRuntime';
+import BibliotecaC from '../pages/topicos/bibliotecaC';
+import DadosC from '../pages/topicos/dadosC';
+import ImplementacaoFuncoesC from '../pages/topicos/implementacaoFuncoesC';
+import GNUCompiler from '../pages/topicos/gcc';
+import AmbienteDesenvolvimento from '../pages/topicos/ambienteDesenvolvimento';
+import AtributosFuncaoGCC from '../pages/topicos/atributosFuncao';
+import AssemblerInline from '../pages/topicos/assemblerInline';
+import RuntimeGCC from '../pages/topicos/runtimeGcc';
+import BibliotecaCNewlib from '../pages/topicos/bibliotecaCNewlib';
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/teste' element={<Teste />} />
+                <Route path='/topicos/acessoMemoria' element={<AcessoMemoria />} />
+                <Route path='/topicos/arquitetura' element={<Arquitetura />} />
+                <Route path='/topicos/conjuntoInstrucoes' element={<ConjuntoInstrucoes />} />
+                <Route path='/topicos/salto' element={<InstrucoesSalto />} />
+                <Route path='/topicos/alu' element={<InstrucoesALU />} />
+                <Route path='/topicos/reinicioExcecoes' element={<ReinicioExcecoes />} />
+                <Route path='/topicos/coprocessadores' element={<Coprocessadores />} />
+                <Route path='/topicos/thumb' element={<ConjuntoInstrucoesThumb />} />
+                <Route path='/topicos/saltoThumb' element={<InstrucoesSaltoThumb />} />
+                <Route path='/topicos/aritmeticas' element={<OperacoesAritmeticas />} />
+                <Route path='/topicos/loadStore' element={<InstrucoesAcessoMemoria />} />
+                <Route path='/topicos/thumb2' element={<ConjuntoInstrucoesThumb2 />} />
+                <Route path='/topicos/execucaoThumb' element={<ExecucaoConjuntoInstrucoesThumb />} />
+                <Route path='/topicos/ambienteGnu' element={<AmbienteGNU />} />
+                <Route path='/topicos/assembler' element={<AssemblerGNU />} />
+                <Route path='/topicos/simbolos' element={<Simbolos />} />
+                <Route path='/topicos/expressoes' element={<Expressoes />} />
+                <Route path='/topicos/definicaoSecoes' element={<DefinicaoSecoes />} />
+                <Route path='/topicos/atribuicaoSimbolos' element={<AtribuicaoSimbolos />} />
+                <Route path='/topicos/introducaoDados' element={<IntroducaoDados />} />
+                <Route path='/topicos/codigoCondicional' element={<CodigoCondicional />} />
+                <Route path='/topicos/enderecoAtual' element={<PosicionarEnderecoAtual />} />
+                <Route path='/topicos/macrosRepeticoes' element={<MacrosRepeticoes />} />
+                <Route path='/topicos/gnuld' element={<GNULD />} />
+                <Route path='/topicos/gdb' element={<DepuradorGNU />} />
+                <Route path='/topicos/execucaoProcesso' element={<ExecucaoControleProcesso />} />
+                <Route path='/topicos/breakpoints' element={<Breakpoints />} />
+                <Route path='/topicos/variaveisMemoria' element={<VariaveisConteudoMemoria />} />
+                <Route path='/topicos/interfaceUsuario' element={<ControleInterfaceUsuario />} />
+                <Route path='/topicos/placaDesenvolvimento' element={<PlacaDesenvolvimento />} />
+                <Route path='/topicos/gpio' element={<PortasEntradaSaidaGPIO />} />
+                <Route path='/topicos/gerenciadorInterrupcoes' element={<GerenciadorInterrupcoes />} />
+                <Route path='/topicos/temporizadores' element={<Temporizadores />} />
+                <Route path='/topicos/uarts' element={<UARTs />} />
+                <Route path='/topicos/implementacaoFuncoesC' element={<ImplementacaoLinguagemC />} />
+                <Route path='/topicos/cRuntime' element={<CRuntime />} />
+                <Route path='/topicos/bibliotecaC' element={<BibliotecaC />} />
+                <Route path='/topicos/dadosC' element={<DadosC />} />
+                <Route path='/topicos/implementacaoFuncoesC' element={<ImplementacaoFuncoesC />} />
+                <Route path='/topicos/gcc' element={<GNUCompiler />} />
+                <Route path='/topicos/ambienteDesenvolvimento' element={<AmbienteDesenvolvimento />} />
+                <Route path='/topicos/atributosFuncao' element={<AtributosFuncaoGCC />} />
+                <Route path='/topicos/assemblerInline' element={<AssemblerInline />} />
+                <Route path='/topicos/runtimeGcc' element={<RuntimeGCC />} />
+                <Route path='/topicos/bibliotecaCNewlib' element={<BibliotecaCNewlib />} />
             </Routes>
         </BrowserRouter>
     );
