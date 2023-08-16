6456780316705108285fabc4702e5b04f8e111d3
Lucas Fengz <lucasfeng83@gmail.com>
2023-08-15 21:49:30 -0300

    atualizando link

--- libproc/src/pages/home/index.tsx
+++ libproc/src/pages/home/index.tsx
@@ -10,7 +10,7 @@ function Page(){
           <h1>LIBPROC</h1>
           <h2>Teoria e questões da disciplina Laboratório de Processadores!</h2>
           <h2>Utilize nosso Emulador ARM! </h2>
-          <a href="https://react-arm-emulator-bkiputr02-fengyungz.vercel.app/">Clique aqui! para testar nosso Emulador ARM!</a>
+          <a href="https://react-arm-emulator.vercel.app/">Clique aqui! para testar nosso Emulador ARM!</a>
         </Container>
         </>
     )
